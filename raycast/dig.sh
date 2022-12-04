8ed47acd89b703f7f26a4f24dfc1a18891dbbf7f
Aris Ripandi <riipandi@gmail.com>
2022-12-05 04:21:04 +0700

    add dig raycast script

---
+++ raycast/dig.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Dig
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 👀
+# @raycast.packageName DNS Lookup Utility
+# @raycast.argument1 { "type": "text", "placeholder": "domain" }
+
+# Documentation:
+# @raycast.description Find DNS Record
+# @raycast.author Aris Ripandi
+# @raycast.authorURL https://github.com/riipandi
+
+dig +noall +answer $1
