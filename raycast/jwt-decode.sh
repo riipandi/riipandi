824c2be0a48a02a257857e7d2d3626e902fbab9e
Aris Ripandi <riipandi@gmail.com>
2023-03-18 03:56:38 +0700

    add Raycast JWT Decode script

---
+++ raycast/jwt-decode.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Decode JWT Token
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🗝️
+# @raycast.packageName JWT Decode
+# @raycast.argument1 { "type": "text", "placeholder": "token" }
+
+# Documentation:
+# @raycast.description Decode JWT Token
+# @raycast.author Aris Ripandi
+# @raycast.authorURL https://github.com/riipandi
+
+jq -R 'split(".") | .[0] | @base64d | fromjson' <<< "$1"
+jq -R 'split(".") | .[1] | @base64d | fromjson' <<< "$1"
